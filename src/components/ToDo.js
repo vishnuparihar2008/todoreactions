82f7cab502032cdde85e282920fb499694549667
Vishnu Parihar <vishnuparihar.delhi5@gmail.com>
2026-05-30 02:26:36 +0530

    Added Components

---
+++ src/components/ToDo.js
@@ -0,0 +1,12 @@
+import ToDoElements from "./ToDoElements";
+
+function ToDo() {
+    return (
+        <div className="todo">
+            <h1>Hello, ToDo!</h1>
+            <ToDoElements />
+        </div>
+    );
+}
+
+export default ToDo;
---
+++ src/components/ToDoElements.js
@@ -0,0 +1,9 @@
+function ToDoElements() {
+    return (
+        <div className="todoelements">
+            <h1>Hello, ToDoElements!</h1>
+        </div>
+    );
+}
+
+export default ToDoElements;
