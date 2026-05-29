3afffa191f783cac882ae6ae4f028895c8121705
Vishnu Parihar <vishnuparihar.delhi5@gmail.com>
2026-05-30 03:03:24 +0530

    Created Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import './compStyles/Header.css'
+import logo from '../favicon.png';
 
 function Header() {
     return (
         <div className="header">
-            <h1>Hello, Header!</h1>
+            <img src={logo} alt="✓" />
+            <h1>THE TO DO'S LIST</h1>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"></button>
+                <ul class="dropdown-menu">
+                    <li><button class="dropdown-item">Action</button></li>
+                    <li><button class="dropdown-item">Another action</button></li>
+                    <li><button class="dropdown-item">Something else here</button></li>
+                </ul>
+            </div>
         </div>
     );
 }
